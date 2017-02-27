569af96ed432a1e6e09c381a685c56fbba76565b
Daneel Yaitskov <diaitskov@egnyte.com>
2017-02-27 15:48:45 +0100

    master check by row

--- src/main/java/org/dan/cipher/demo/CrisCrossField.java
+++ src/main/java/org/dan/cipher/demo/CrisCrossField.java
@@ -7,4 +7,27 @@ import java.util.Map;
 @Builder
 public class CrisCrossField {
     private final Map<CellAddress, CellState> cellMap;
+    private final int width;
+    private final int height;
+
+    private boolean isVictoryByRow() {
+        CellState first = null;
+        nextRow:
+        for (int iRow = 0; iRow < width; ++iRow) {
+            for (int iCol = 0; iCol < height; ++iCol) {
+                CellState state = cellMap.get(CellAddress.builder()
+                        .row(iRow).column(iCol).build());
+                if (state == null) {
+                    continue nextRow;
+                } else if (first == null) {
+                    first = state;
+                } else if (first != state) {
+                    continue nextRow;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
